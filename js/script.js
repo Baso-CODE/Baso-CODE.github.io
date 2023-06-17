ae54586f7564605169651618f616188624459200
Baso-CODE <basoafrizamuhafriza@gmail.com>
2023-06-17 13:50:34 +0800

    update pertama

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//toggle class active
+// const navbarNav = document.querySelector
+// ('.navbar-nav');
+// document.querySelector('#hamburger-menu'). onclick = () => {
+//     navbarNav.classList.toggle('active');
+// };
+//toggle class acctive untuk search form
+const searchForm = document.querySelector( '.search-form');
+const searchBox = document.querySelector( '#search-box');
+
+
+
+document.querySelector( "#search-button" ).onclick = () => {
+    searchForm.classList.toggle('active');
+};
+
+
+// klick di luar sidebar 
+// const hamburger = document.querySelector
+// ('hamburger-menu');
+// document.addEventListener ('click',function(e) {
+//     if (!hamburger.contains(e.target)&& !navbarNav.contains(e.target)) {
+//         navbarNav.classList.remove('active');
+
+//     }   
+
+// });
+
+
+
+
+
